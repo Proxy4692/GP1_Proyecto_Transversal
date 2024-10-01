8c42309761083c5e9ca607b453238e67403b9d85
Rodrigo <99767343+Proxy4692@users.noreply.github.com>
2024-10-01 20:29:50 -0300

    Creacion de Proyecto

---
+++ GP1_ProjectoTransversal/src/main/java/universidad/Entidades/Inscripcion.java
@@ -0,0 +1,68 @@
+package universidad.Entidades;
+
+import static grp1tp4.Colegio.listaAlumnos;
+import static grp1tp4.Colegio.listaMaterias;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Inscripcion{
+    private LocalDate fecha;
+    private int codigoMat;
+    private int codigoAlu;
+    private String sel;
+
+    public Inscripcion(LocalDate fecha, int codigoMat, int codigoAlu) {
+        this.fecha = fecha;
+        this.codigoMat = codigoMat;
+        this.codigoAlu = codigoAlu;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMateria() {
+        sel= "";
+        for (Materia materia : listaMaterias) {
+            if(materia.getIdMateria() == codigoMat){
+                sel= materia.getNombre();
+            }           
+        }
+        return sel;
+    }
+
+    public String getAlumno() {
+        sel= "";
+        for (Alumno alumno : listaAlumnos) {
+            if(alumno.getLegajo() == codigoAlu){
+                sel= alumno.toString();
+            }           
+        }
+        return sel;
+    }
+
+    public int getCodigoMat() {
+        return codigoMat;
+    }
+
+    public void setCodigoMat(int codigoMat) {
+        this.codigoMat = codigoMat;
+    }
+
+    public int getCodigoAlu() {
+        return codigoAlu;
+    }
+
+    public void setCodigoAlu(int codigoAlu) {
+        this.codigoAlu = codigoAlu;
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion{" + "fecha=" + fecha.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ", codigoMat=" + codigoMat + ", codigoAlu=" + codigoAlu + '}';
+    }
+}
