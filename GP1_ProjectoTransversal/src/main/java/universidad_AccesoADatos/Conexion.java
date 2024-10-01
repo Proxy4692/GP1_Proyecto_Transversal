dcca8c870852f0808446e4f6ec63a9297f46d236
Rodrigo <99767343+Proxy4692@users.noreply.github.com>
2024-10-01 20:48:52 -0300

    Update Conexion.java

--- GP1_ProjectoTransversal/src/main/java/universidad_AccesoADatos/Conexion.java
+++ GP1_ProjectoTransversal/src/main/java/universidad_AccesoADatos/Conexion.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package universidad_AccesoADatos;
 
-/**
- *
- * @author Rodrigo Felipe
- */
-public class Conexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexion{
+    private static final String URL="jdbc:mysql://localhost/";
+    private static final String DB="universidadulp";
+    private static final String USUARIO="root";
+    private static String PASSWORD="";
     
+    private static Connection connection;
+    
+    //Constructor Method
+    private Conexion(){
+        public static Connection getConectConnection(){
+            if(connection == null){
+                try{
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    //Setup the connection with the DB
+                    connection = DriverManager.getConnection(URL+DB = "?useLegacyDatetimeCode=false&serverTimezone=UTC" + "&user=" + USUARIO + "&password=" + PASSWORD);
+                }catch(SQLException ex){
+                    JOptionPane.showMessageDialog(null, "Error al conectarse a la BD " + ex.getMessage());
+                }catch(ClassNotFoundException ex){
+                    JOptionPane.showMessageDialog(null, "Error al cargar los Drivers " + ex.getMessage());
+                }
+            }
+            return connection;
+        }
+    }
 }
