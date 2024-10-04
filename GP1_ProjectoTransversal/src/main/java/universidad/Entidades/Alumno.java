c77ae035486ba1d8cea4909803d3391a129ae921
Alabrato <andresgarbs@gmail.com>
2024-10-03 21:10:08 -0300

    arreglado alumno(??? nose q le paso lol

--- GP1_ProjectoTransversal/src/main/java/universidad/Entidades/Alumno.java
+++ GP1_ProjectoTransversal/src/main/java/universidad/Entidades/Alumno.java
@@ -1,146 +1,61 @@
 package universidad.Entidades;
 
-import java.time.LocalDate;
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-import java.util.HashSet;
-import java.util.Iterator;
-=======
->>>>>>> f4f9100764ef7870391c20d041b8b52b8916dcdd
->>>>>>> Stashed changes
+public class Inscripcion{
+    public int idInscripcion;
+    public Alumno alumno;
+    public Materia materia;
+    public double nota;
 
-public class Alumno{
-    private int idAlumno;
-    private String apellido;
-    private String nombre;
-    private LocalDate fechaNac;
-    private boolean activo;
-
-    public Alumno() {
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
     }
 
-    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
-        this.idAlumno = idAlumno;
-        this.apellido = apellido;
-        this.nombre = nombre;
-        this.fechaNac = fechaNac;
-        this.activo = activo;
-<<<<<<< Updated upstream
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
     }
 
-=======
+    public Inscripcion() {
     }
 
-<<<<<<< HEAD
-    public Alumno() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Inscripcion(double nota) {
+        this.nota = nota;
     }
 
-    public int getLegajo() {
-        return legajo;
-=======
->>>>>>> Stashed changes
-    public Alumno(String apellido, String nombre, LocalDate fechaNac, boolean activo) {
-        this.apellido = apellido;
-        this.nombre = nombre;
-        this.fechaNac = fechaNac;
-        this.activo = activo;
-<<<<<<< Updated upstream
-=======
->>>>>>> f4f9100764ef7870391c20d041b8b52b8916dcdd
->>>>>>> Stashed changes
+    public int getIdInscripcion() {
+        return idInscripcion;
     }
 
-    public int getIdAlumno() {
-        return idAlumno;
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
     }
 
-    public void setIdAlumno(int idAlumno) {
-        this.idAlumno = idAlumno;
+    public Alumno getAlumno() {
+        return alumno;
     }
 
-    public String getApellido() {
-        return apellido;
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public Materia getMateria() {
+        return materia;
     }
 
-    public String getNombre() {
-        return nombre;
+    public void setMateria(Materia materia) {
+        this.materia = materia;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public double getNota() {
+        return nota;
     }
 
-    public LocalDate getFechaNac() {
-        return fechaNac;
+    public void setNota(double nota) {
+        this.nota = nota;
     }
-
-    public void setFechaNac(LocalDate fechaNac) {
-        this.fechaNac = fechaNac;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    } 
-
-    @Override
-    public String toString(){
-        return nombre + " " + apellido;
-    }
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
     
-    public int cantidadMaterias(){
-        Iterator it = listaMaterias.iterator();
-        int cantMaterias = 0;
-        while (it.hasNext()) {            
-            cantMaterias += 1;           
-        }
-        return cantMaterias;
-    }
-
-    public void setIdAlumno(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void setDni(int aInt) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void setFechaNacimiento(LocalDate toLocalDate) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void setEstado(boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public int getIdAlumno() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String getFechaNacimiento() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public int getDni() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public boolean isEstado() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-=======
->>>>>>> f4f9100764ef7870391c20d041b8b52b8916dcdd
->>>>>>> Stashed changes
 }
