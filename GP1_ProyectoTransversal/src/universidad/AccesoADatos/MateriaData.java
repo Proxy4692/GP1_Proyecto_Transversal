e5b2ad9a3cd64d9c877210d00050f6efd9f4baf4
Rodrigo <99767343+Proxy4692@users.noreply.github.com>
2024-10-04 19:26:18 -0300

    Reestructurado por errores

---
+++ GP1_ProyectoTransversal/src/universidad/AccesoADatos/MateriaData.java
@@ -0,0 +1,16 @@
+package universidad.AccesoADatos;
+
+import java.sql.Connection;
+import universidad.Entidades.Materia;
+
+public class MateriaData {
+    private Connection con = null;
+    
+    public MateriaData(){
+        this.con = Conexion.getConexion();
+    }
+    
+    public void guardarMateria(Materia materia){
+        
+    }
+}
