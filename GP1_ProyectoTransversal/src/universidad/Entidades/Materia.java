363142a9569d3046aaf417b803f6d50af4ee490f
Alabrato <andresgarbs@gmail.com>
2024-10-08 19:16:41 -0300

    class Materia
    como en la guia

--- GP1_ProyectoTransversal/src/universidad/Entidades/Materia.java
+++ GP1_ProyectoTransversal/src/universidad/Entidades/Materia.java
@@ -1,17 +1,26 @@
 package universidad.Entidades;
 
-import static listaMaterias;
-
 public class Materia {
     
     private int idMateria;
     private String nombre;
-    private int anio;
+    private int anioMateria;
+    boolean activo;
 
-    public Materia(int idMateria, String nombre, int anio) {
+    public Materia() {
+    }
+
+    public Materia(int idMateria, String nombre, int anioMateria, boolean activo) {
         this.idMateria = idMateria;
         this.nombre = nombre;
-        this.anio = anio;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public Materia(String nombre, int anioMateria, boolean activo) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
     }
 
     public int getIdMateria() {
@@ -30,26 +39,27 @@ public class Materia {
         this.nombre = nombre;
     }
 
-    public int getAnio() {
-        return anio;
+    public int getAnioMateria() {
+        return anioMateria;
     }
 
-    public void setAnio(int anio) {
-        this.anio = anio;
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
     }
 
-    public boolean crearMateria(Materia m){
-        for (Materia materia : listaMaterias) {
-            if(materia.equals(m)){
-                listaMaterias.add(m);
-                return true;
-            }           
-        }
-        return false;      
+    public boolean isActivo() {
+        return activo;
     }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
+
     
     @Override
     public String toString() {
-        return  nombre + " (Año: " + anio+ ")";
+        return  nombre + " (Año: " + anioMateria+ ")";
     }
 }
