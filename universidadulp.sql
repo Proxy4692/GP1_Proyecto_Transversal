f569770caf13fd4568b0432df66b11d89f50a967
dihegogabriel <diegabrielgallardo@gmail.com>
2024-10-18 08:25:53 -0300

    Update universidadulp.sql

--- universidadulp.sql
+++ universidadulp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-10-2024 a las 22:02:03
+-- Tiempo de generación: 18-10-2024 a las 12:37:39
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -36,17 +36,6 @@ CREATE TABLE `alumno` (
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `alumno`
---
-
-INSERT INTO `alumno` (`idAlumno`, `dni`, `apellido`, `nombre`, `fechaNacimiento`, `estado`) VALUES
-(1, 32478489, 'Luna', 'Ignacio', '1981-04-25', 1),
-(2, 39000000, 'Perez', 'Pedro', '1978-06-01', 1),
-(3, 29478489, 'Coria', 'Juan', '1984-02-17', 1),
-(4, 38478489, 'Maranguelo', 'Orlando', '1982-09-06', 1),
-(5, 40478489, 'Aldao', 'Marta', '1999-09-09', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -60,24 +49,6 @@ CREATE TABLE `inscripcion` (
   `idMateria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `inscripcion`
---
-
-INSERT INTO `inscripcion` (`idInscripto`, `nota`, `idAlumno`, `idMateria`) VALUES
-(1, 7.5, 1, 6),
-(2, 9, 1, 8),
-(3, 8.5, 1, 10),
-(4, 6.5, 2, 7),
-(5, 7, 2, 9),
-(6, 8.9, 2, 10),
-(7, 9.5, 3, 6),
-(8, 7, 3, 8),
-(10, 6, 4, 7),
-(11, 4.5, 4, 8),
-(13, 8, 5, 7),
-(14, 6.5, 5, 8);
-
 -- --------------------------------------------------------
 
 --
@@ -90,17 +61,6 @@ CREATE TABLE `materia` (
   `anioMateria` int(11) NOT NULL,
   `estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `materia`
---
-
-INSERT INTO `materia` (`idMateria`, `nombre`, `anioMateria`, `estado`) VALUES
-(6, 'Laboratorio de Programacion', 2, 1),
-(7, 'Matematica Aplicada', 3, 1),
-(8, 'Ingles', 1, 1),
-(9, 'Programacion Web', 3, 1),
-(10, 'Base de datos', 1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -135,7 +95,7 @@ ALTER TABLE `materia`
 -- AUTO_INCREMENT de la tabla `alumno`
 --
 ALTER TABLE `alumno`
-  MODIFY `idAlumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idAlumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `inscripcion`
